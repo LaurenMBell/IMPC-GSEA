3ec447e25294ad54f843048eee821e4dd6d9e725
LaurenMBell <bell.laurenmar@gmail.com>
2026-04-19 18:10:37 -0700

    parameters page visually completed

--- App.R
+++ App.R
@@ -1,61 +1,110 @@
-library("devtools")
-pak::pak("GSEA-MSigDB/GSEA_R")
+# 4/16/26 last update
+
 library(shiny)
 library(bslib)
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
-#most of the layout if based off this file:
-#source(system.file('extdata', 'Run.GSEA.R', package = 'GSEA'))
+# most of the layout is based off this file:
+# source(system.file('extdata', 'Run.GSEA.R', package = 'GSEA'))
+source("gene_set_val.R")
+
+pre = TRUE #tick for when parameters are submitted GET RID OF THIS LATER
 
 ui <- page_navbar(
   
-  title = "GSEA-IMPC | Morgun-Shulzhenko Lab",
-  
-  sidebar = sidebar(
-    title = "Selection menu", 
-    width = 350,
-    
-    fileInput("user_gene_set", 
-              label = "Upload your RNK-formatted gene set:"
-    ), #closes file input
-    
-    textOutput("gene_set_check"), 
-    
-    numericInput("permutations", 
-                 label = "Number of permutations: ", 
-                 value = 1000, 
-                 min = 1, 
-                 max = 10000
+  title = "IMPC-GSEA | Morgun-Shulzhenko Lab",
+
+  # PARAMETER + GSEA PAGE ++++++++++++++++++++++++++++++++++++
+  nav_panel(
+    "GSEA",
+    layout_sidebar(
+      sidebar = sidebar(
+        card_header("User Selected Parameters"),
+        
+        #=== upload file and make sure it is formatted correctly ====
+        fileInput("user_gene_set",
+                  label = "Upload your RNK-formatted gene set file:"
+                  # ADD DROP HEADER CHECK OPTION
+        ),
+        textOutput("gene_set_check"),
+        
+        #=== collapse gene set to gene symbols, must provide CHIP file ====
+        checkboxInput("collapsed",
+                      label = "Collapse gene set to gene symbols?",
+                      value = FALSE),
+        conditionalPanel(
+          condition = "input.collapsed == true",
+          fileInput("chip_file", "Upload CHIP file:"),
+          textOutput("CHIP_check")
+        ),
+        
+        #=== number of permutations ====
+        numericInput("permutations",
+                     label = "Number of permutations: ",
+                     value = 10000,
+                     min = 1,
+                     max = 100000
+        ),
+        
+        #=== null model type ====
+        selectInput(
+          "null_model",
+          "Select method of estimating enrichment score significance:",
+          c("Gene label permutation" = "gene_label_permutation",
+            "Gamma distribution approximation (blitzGSEA)" = "blitzgsea",
+            "Estimation via moments of test statistic (npGSEA)" = "npgsea")
+        ), 
+        
+        actionButton("run", "Run GSEA with selected parameters", 
+                     style = "color: white; background-color: #7BC950; border-color: #F7F2F0;")
+        
+      ),
+      
+      # main panel — results go here
+      div(
+        if (pre) {
+          p("Waiting for user to select parameters")
+        } else {
+          p("Running GSEA with selected parameters!")
+          # figure out how to do a cool animation here !!
+        }
+      )
     )
-  ), #closes side bar
-  
-  card(
-    title = "User Selected Parameters", 
-    label = "selected_params",
   )
   
 ) # closes ui
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 server <- function(input, output) {
   
-  output$gene_set_check <- renderText ({
+  # ==== check input gene set file formatting =====
+  output$gene_set_check <- renderText({
     req(input$user_gene_set)
     
     file_path <- input$user_gene_set$datapath
     
     if (user_set_check(file_path) == TRUE) {
-      "No errors detected!!"
+      "No errors detected :)"
     } else {
-      "Incorrect format"
+      "Incorrect format :("
     }
-    
-  }) #closes gene_set_check 
+  })
   
-}
+  # ==== check input CHIP file formatting =====
+  output$CHIP_check <- renderText({
+    req(input$chip_file)
+    
+    file_path <- input$chip_file$datapath
+    
+    if (user_CHIP_check(file_path) == TRUE) { 
+      "No errors detected :0"
+    } else {
+      "Incorrect format :P"
+    }
+  })
+  
+} # closes server
 
 shinyApp(ui = ui, server = server)
-
-
-
---
+++ gene_set_val.R
@@ -0,0 +1,26 @@
+# 4/11/26 script for checking user gene sets in the GSEA tool, currently placeholder
+
+user_set_check <- function(infile) {
+  #TO DO: check file type, and make sure its a valid 
+  df <- read_csv(infile)
+  
+  if (ncol(df) != 2) { 
+    return(FALSE)
+    }
+  
+  #check for tab-delim
+  
+  return(TRUE)
+  
+}
+
+user_CHIP_check <- function(infile) {
+  #TODO: double check CHIP formatting
+  return (TRUE)
+}
+
+drop_header <- function(infile) {
+  #drop header of input files for RNK or CHIP
+  
+}
+
